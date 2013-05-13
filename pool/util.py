9ca8887eb4eebca678d62ee05388937f7f266f66
Inseok Lee <dlunch@gmail.com>
2013-05-13 16:56:55 +0900

    Add bytes to/from long conversion

--- pool/util.py
+++ pool/util.py
@@ -2,51 +2,90 @@ import struct
 import binascii
 import hashlib
 
+# From https://github.com/dlitz/pycrypto/blob/master/lib/Crypto/Util/number.py
+# (Public domain)
+b = bytes
+def long_to_bytes(n, blocksize=0):
+    """long_to_bytes(n:long, blocksize:int) : string
+    Convert a long integer to a byte string.
+
+    If optional blocksize is given and greater than zero, pad the front of the
+    byte string with binary zeros so that the length is a multiple of
+    blocksize.
+    """
+    # after much testing, this algorithm was deemed to be the fastest
+    s = b('')
+    n = long(n)
+    pack = struct.pack
+    while n > 0:
+        s = pack('>I', n & 0xffffffffL) + s
+        n = n >> 32
+    # strip off leading zeros
+    for i in range(len(s)):
+        if s[i] != b('\000')[0]:
+            break
+    else:
+        # only happens when n == 0
+        s = b('\000')
+        i = 0
+    s = s[i:]
+    # add back some pad bytes. this could be done more efficiently w.r.t. the
+    # de-padding being done above, but sigh...
+    if blocksize > 0 and len(s) % blocksize:
+        s = (blocksize - len(s) % blocksize) * b('\000') + s
+    return s
+
+def bytes_to_long(s):
+    """bytes_to_long(string) : long
+    Convert a byte string to a long integer.
+
+    This is (essentially) the inverse of long_to_bytes().
+    """
+    acc = 0L
+    unpack = struct.unpack
+    length = len(s)
+    if length % 4:
+        extra = (4 - length % 4)
+        s = b('\000') * extra + s
+        length = length + extra
+    for i in range(0, length, 4):
+        acc = (acc << 32) + unpack('>I', s[i:i+4])[0]
+    return acc
+
+
 base58_data = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
 def base58_encode(data):
     result = ''
-    value = long(binascii.hexlify(data), 16)
+    value = bytes_to_long(data)
     while value > 0:
         value, c = divmod(value, 58)
         result += base58_data[c]
 
-    for i in data:
-        if i == '\x00':
-            result += base58_data[0]
-            continue
-        break
-
     return result[::-1]
 
 
-def base58_decode(data):
+def base58_decode(data, size):
     value = 0
     mul = 1
     for i in data[::-1]:
         value += base58_data.find(i) * mul
         mul *= 58
 
-    result = ''
-    for i in data:
-        if i == base58_data[0]:
-            result += '\x00'
-            continue
-        break
-    result += binascii.unhexlify(hex(value)[2:].rstrip('L'))
-
-    return result
+    return long_to_bytes(value, size)
 
 
 def address_to_pubkey(address):
-    data = base58_decode(address)
+    data = base58_decode(address, 25)
     return data[1:21]
 
 
 def pubkey_to_address(pubkey, net):
     data = net.address_prefix + pubkey
     checksum = hashlib.sha256(hashlib.sha256(data).digest()).digest()[:4]
-    return base58_encode(data + checksum)
+    result = base58_encode(data + checksum)
+
+    return base58_data[0]*(34-len(result)) + result
 
 
 def encode_height(height):
