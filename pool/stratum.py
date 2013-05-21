d8cdc757c4c4e78df373df09858c2dd55fdcdffb
Inseok Lee <dlunch@gmail.com>
2013-05-21 15:02:54 +0900

    Implement mining subscription

--- pool/stratum.py
+++ pool/stratum.py
@@ -1,15 +1,23 @@
 import gevent
 import gevent.event
 import logging
+import struct
+import binascii
 logger = logging.getLogger('Stratum')
 
 import jsonrpc
+import config
 
 
 class Stratum(object):
+    seq = 0
+
     def __init__(self, file, work):
         self.file = file
         self.work = work
+        self.extranonce1 = Stratum.seq ^ 0xdeadbeef
+        self.difficulty = config.target_difficulty
+        Stratum.seq += 1
 
     def _send_stratum_message(self, message):
         logger.debug('Stratum send: %s' % message)
@@ -19,9 +27,13 @@ class Stratum(object):
     def block_pusher(self):
         event = gevent.event.Event()
         while True:
-            request = jsonrpc.create_request('mining.notify',
-                                             self.work.get_stratum_work())
-            self._send_stratum_message(request)
+            self._send_stratum_message(jsonrpc.create_request(
+                                       'mining.set_difficulty',
+                                       [self.difficulty]))
+
+            self._send_stratum_message(jsonrpc.create_request(
+                                       'mining.notify',
+                                       self.work.get_stratum_work()))
 
             self.work.add_longpoll_event(event)
             event.wait()
@@ -41,4 +53,9 @@ class Stratum(object):
 
     def mining_subscribe(self, uri, params):
         self.pusher = gevent.spawn(self.block_pusher)
-        return {}
+        nonce1 = binascii.hexlify(struct.pack('<I', self.extranonce1))
+        return [['mining.notify', 'ae6812eb4cd7735a302a8a9dd95cf71f'],
+                nonce1, config.extranonce2_size]
+
+    def mining_authorize(self, uri, params):
+        return True
