e449d4e2b9a9ef08eb419af566eedd23280fd90b
Inseok Lee <dlunch@gmail.com>
2013-05-21 16:12:38 +0900

    PEP8

--- pool/coinbase_transaction.py
+++ pool/coinbase_transaction.py
@@ -5,6 +5,7 @@ import util
 from transaction import Transaction
 import config
 
+
 class CoinbaseTransaction(object):
     def __init__(self, block_template, generation_pubkey):
         #BIP 0034
@@ -29,18 +30,18 @@ class CoinbaseTransaction(object):
 
         output_script = '\x76\xa9\x14' + generation_pubkey + '\x88\xac'
 
-        result = '\x01\x00\x00\x00' # Version
-        result += '\x01' # In counter(1)
-        result += '\x00' * 32 # Input(None)
-        result += '\xff\xff\xff\xff' # Input Index (None)
+        result = '\x01\x00\x00\x00'  # Version
+        result += '\x01'  # In counter(1)
+        result += '\x00' * 32  # Input(None)
+        result += '\xff\xff\xff\xff'  # Input Index (None)
         result += util.encode_size(len(coinbase_script))
         result += coinbase_script
-        result += '\xff\xff\xff\xff' # Sequence
-        result += '\x01' # Out counter(1)
+        result += '\xff\xff\xff\xff'  # Sequence
+        result += '\x01'  # Out counter(1)
         result += struct.pack('<Q', coinbase_value)
         result += util.encode_size(len(output_script))
         result += output_script
-        result += '\x00\x00\x00\x00' # Lock time
+        result += '\x00\x00\x00\x00'  # Lock time
 
         self.raw_tx = result
 
